1cf1d7f4af809f8e76b4f6463c392ffbbdd4c7a8
Alejandro Guzman <a.guillermoguzman@gmail.com>
2018-01-21 13:25:00 -0500

    app to response to twilio message

---
+++ app.py
@@ -0,0 +1,15 @@
+from sanic import Sanic
+from sanic.response import text
+
+
+app = Sanic()
+
+
+@app.route('/sms', methods=['POST'])
+async def hello(request):
+    message_body = request.form['Body'][0]
+    return text('You said: {}'.format(message_body))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
